8b076b10c3b5ebe5c16cd4b8241c018e50d86e49
Barış İÇEN <icenbaris50@gmail.com>
2025-07-09 23:13:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Active Passive</title>
+    <title>PHP PDO Active Passive</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
@@ -10,23 +10,23 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <!-- We created a table to show our data-->
+                <!-- Verilerimizi göstermek için bir tablo oluşturduk-->
                 <table class="table table-sm table-hover">
                     <br>
-                    <h4 style="text-align:center;">Active Passive Editing of Data</h4>
+                    <h4 style="text-align:center;">Verilerin Aktif Pasif Düzenlemesi</h4>
                     <thead>
                         <th>ID</th>
-                        <th>Title</th>
-                        <th>Content</th>
+                        <th>Başlık</th>
+                        <th>Açıklama</th>
                         <th>Active</th>                       
                     </thead>
                     <?php
-                    include('fonc.php'); // We include our database on our pages
-                    $query = $connect->prepare("Select * from products"); // We write our query to sort our data by id
-                    $query->execute(); // We start the query
+                    include('fonc.php'); // Veritabanımızı sayfalarımıza dahil ediyoruz
+                    $query = $connect->prepare("Select * from products"); // Verilerimizi id ye göre sıralamak için sorgumuzu yazıyoruz
+                    $query->execute(); // Sorguyu başlatıyoruz
 
-                    while ($result = $query->fetch())  // We returned it with a while loop to sort our data
-                            {      // While Start , We sort our data between the start of while and the end of while
+                    while ($result = $query->fetch())  // Verilerimizi sıralamak için bir süre döngüsü ile iade ettik
+                            {      // While Start , Verilerimizi while'in başlangıcı ile while'ın sonu arasında sıralarız
                     ?>
                         <tbody>
                             <td><?= $result['id']?></td>
@@ -34,44 +34,44 @@
                             <td><?= $result['content']?></td>
                             <td>
                                 <label class="switch">
-                                    <!-- We added id and active (1 or 0) information to our checkbox -->
+                                    <!-- id ve active (1 veya 0) bilgileri onay kutumuza ekledik -->
                                     <input type="checkbox" id='<?php echo $result['id'] ?>'
                                     class="ActivePassive" <?php echo $result['active'] == 1 ? 'checked' : '' ?> />  
-                                    <!--Let's pay attention to what is written to the class in the input. We will send this data to our file named "activedata.js" -->
+                                    <!--Girişte class a yazılanlara dikkat edelim. Bu verileri "activedata.js" adlı dosyamıza göndereceğiz -->
                                      <span class="slider"></span>
                                 </label>
                             </td>                            
                             </tbody>
                             <?php
-                        }  // While End
+                        }  // While bitişi
 
                         ?>
                     </table>
-                    <h2 style="color:red; text-align:center;" id="result"></h2> <!-- Our header to report errors and results -->
+                    <h2 style="color:red; text-align:center;" id="result"></h2> <!-- Hataları ve sonuçları bildirmek için başlığımız -->
                 </div>
 
                 <div class="col-md-6">
-                <!--Sorting Data in the Database -->
+                <!--Veritabanında Verileri Sıralama -->
                     <table class="table">
                         <br>
-                        <h4 style="text-align:center;">Products</h4>
-                        <h6>Sorting Data in the Database</h6>
+                        <h4 style="text-align:center;">Ürünler</h4>
+                        <h6>Veritabanındaki Verileri Sıralama</h6>
 
                         <thead class="thead-dark">
                              <tr>
                                 <th scope="col">id</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">Content</th>                                
+                                <th scope="col">Başlık</th>
+                                <th scope="col">Açıklama</th>                                
                              </tr>
                         </thead>
                     <tbody>
                         <?php
                             $query = $connect->prepare("SELECT * FROM products where active=1"); 
-            // We sort your data according to Active-Passive Status. State = 1 shows the data. Status = 0 We are not showing data
+            // Verilerinizi Aktif-Pasif Duruma göre sıralıyoruz. Durum = 1 verileri gösterir. Durum = 0 Veri göstermiyoruz
                             $query->execute(); // query end
 
                                 while ($result = $query->fetch()) 
-                                    { //While Start, We sort our data between the start of while and the end of while
+                                    { //Başlarken, verilerimizi while'in başlangıcı ile while'ın sonu arasında sıralarız
                         ?>
 
                             <tr>
@@ -81,7 +81,7 @@
                             </tr>
 
                             <?php
-                                    } //While End
+                                    } //While bitiş
                             ?>
                         
                     </tbody>
@@ -93,7 +93,7 @@
             </div>
         </div>
         <script src="js/jquery-3.4.1.min.js"></script>
-        <script src="activedata.js"></script> <!-- We get our "activdata.js" file, including-->
-        <link rel="stylesheet" type="text/css" href="css/switch.css"> <!--We include the css file of our Active Passive Button -->
+        <script src="activedata.js"></script> <!-- Dahil olmak üzere "activdata.js" dosyamızı alıyoruz-->
+        <link rel="stylesheet" type="text/css" href="css/switch.css"> <!--Aktif Pasif switch css dosyasını dahil ediyoruz -->
     </body>
     </html>
